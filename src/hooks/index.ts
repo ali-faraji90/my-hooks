50b075bb2d3c901e07c3b148fec6879344e437fd
Ali Faraji <ali.faraji90@gmail.com>
2022-05-15 03:10:33 +0200

    Add exports.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useInputState } from './useInputState'
 export { default as useBoolState } from './useBoolState'
+export { default as useCheckedInputState } from './useCheckedInputState'
